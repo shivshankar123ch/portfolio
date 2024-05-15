636e2cbd39c544d4a6ccadac614cfc7ef2713709
shivshankar <chaudharshivshankarex09@gmail.com>
2024-05-15 11:24:51 -0700

    fourth commit

--- project/settings.py
+++ project/settings.py
@@ -75,11 +75,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
+
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR / 'db.sqlite3',
-}
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'',
+    }
 }
 
 
